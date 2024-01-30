134c3c0b7d0aa1ca5aad953f7188209703b43416
Harshit Singla <harshitashwani@gmail.com>
2024-01-30 16:58:33 +0530

    Just One mistake behind in JS

--- app.js
+++ app.js
@@ -1,63 +1,88 @@
-let levelInfo = document.querySelector('h3');
-
-let start = 'Press any key to start !!!';
-
-let btns = document.getElementsByClassName('box');
-
 let glowSeq = [];
 let userSeq = [];
-
+let levelInfo = document.querySelector('h3');
+let start = 'Press any key to start !!!';
+let over = `Game Over!!! Press any key to continue.`
+let btns = document.getElementsByClassName('box');
+let glowBtn = 0;  // Corrected variable name
 let gameStart = false;
 let currLevel = 0;
 
+document.addEventListener('keypress', function (event) {
+    if (!gameStart) {
+        gameStart = true;
+        levelUp();
+        console.log("Game has started");
+    }
+});
+
+function flashBtn() {
+    console.log("The btn flashes");
+    let randIdx = Math.floor(Math.random() * 4);
+    let btn = btns[randIdx];
+
+    btn.style.backgroundColor = "white";
+    setTimeout(() => {
+        btn.style.backgroundColor = "";
+    }, 250);
+
+    glowSeq.push(`${randIdx}`);
+    glowBtn = randIdx;  // Corrected variable assignment
+    console.log(glowSeq);
+}
 function blink() {
-    if (levelInfo.textContent === start) {
+    if ((levelInfo.textContent === start) || (levelInfo.textContent === over)) {
         if (levelInfo.classList.contains('hidden')) {
             levelInfo.classList.remove('hidden');
-        }
-        else {
+        } else {
             levelInfo.classList.add('hidden');
         }
     }
 }
 
+
 function levelUp() {
-    currLevel++
+    userSeq = [];
+    currLevel++;
+    levelInfo.textContent = `Level ${currLevel}`;
+    levelInfo.classList.remove('hidden');
+    flashBtn();
 }
 
-function flashBtn(btn) {
-    if (btn = btns[0]) {
-        btn.classList.add(`flash0`);
-        setTimeout(() => {
-            btn.classList.remove(`flash0`);
-        }, 1000)
-    }
-    else if (btn = btns[1]) {
-        btn.classList.add(`flash1`);
-        setTimeout(() => {
-            btn.classList.remove(`flash1`);
-        }, 1000)
-    }
-    else if (btn = btns[2]) {
-        btn.classList.add(`flash2`);
-        setTimeout(() => {
-            btn.classList.remove(`flash2`);
-        }, 1000)
-    }
-    else if (btn = btns[3]) {
-        btn.classList.add(`flash3`);
-        setTimeout(() => {
-            btn.classList.remove(`flash3`);
-        }, 1000)
-    }
-}
 
 setInterval(blink, 1000);
-if (levelInfo.textContent === start) {
-    document.addEventListener('keypress', function (event) {
-        levelUp();
-        levelInfo.textContent = `Level ${currLevel}`;
-        levelInfo.classList.remove('hidden');
-        flashBtn(btns[0]);
+
+
+
+if (currLevel === 1) {
+    document.addEventListener('click', function (event) {
+        if (event === glowBtn) {
+            console.log("Correct click!");
+            levelUp();
+        }
     });
+}
+function btnPress() {
+    let btn = this;
+    let btnId = btn.getAttribute("id");
+    userSeq.push(btnId);
+    console.log(userSeq);
+    chkAnswer(currLevel - 1);
+
+}
+let allBtns = document.querySelectorAll(".box");
+for (btn of allBtns) {
+    btn.addEventListener('click', btnPress);
+}
+
+function chkAnswer(idx) {
+    if (userSeq[idx] === glowSeq[idx]) {
+        if (userSeq.length === glowSeq.length) {
+            levelUp();
+        }
+    }
+    else {
+        console.log("Wrong Click!");
+        levelInfo.innerText = over;
+    }
 }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let r = 5;
+let s = "This5";
+let comb = `This${r}`;
+// Now check if s === comb;
+console.log(s == comb); // false
